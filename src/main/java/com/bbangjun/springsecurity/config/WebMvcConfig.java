eac2cf84248d4cc631856998520eb114b53739b1
bbangjun <lucid9dawn@gmail.com>
2023-07-28 23:08:13 +0900

    feat(IndexController): 기본 컨트롤러 메서드 추가 생성

--- src/main/java/com/bbangjun/springsecurity/config/WebMvcConfig.java
+++ src/main/java/com/bbangjun/springsecurity/config/WebMvcConfig.java
@@ -10,10 +10,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         MustacheViewResolver resolver = new MustacheViewResolver();
+
         resolver.setCharset("UTF-8");
-        resolver.setContentType("text/html; charset=UTF-8");
+        resolver.setContentType("text/html;charset=UTF-8");
         resolver.setPrefix("classpath:/templates/");
-        resolver.setSuffix(".html"); // html로 만들어도 mustache가 인식 가능
+        resolver.setSuffix(".html");
 
         registry.viewResolver(resolver);
     }
