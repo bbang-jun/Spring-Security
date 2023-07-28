9a49912e17e16aad38c36f90c897521e2d44973a
bbangjun <lucid9dawn@gmail.com>
2023-07-28 23:24:47 +0900

    feat(SecurityConfig): 스프링 시큐리티 설정을 위한 config 파일 생성

---
+++ src/main/java/com/bbangjun/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.bbangjun.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+}
