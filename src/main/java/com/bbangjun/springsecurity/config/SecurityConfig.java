3cb25c6690ec0088dd419f992a228a9a5d96c02e
bbangjun <lucid9dawn@gmail.com>
2023-07-28 23:26:08 +0900

    feat(SecurityConfig): csrf 방어 적용

--- src/main/java/com/bbangjun/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/bbangjun/springsecurity/config/SecurityConfig.java
@@ -10,5 +10,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
 }
